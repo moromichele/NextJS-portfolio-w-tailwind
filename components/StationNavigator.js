924aa87bdd72578a53dc165b3f1486b5819f21d6
moromichele <moro.michele1997@gmail.com>
2022-08-31 20:22:06 +0200

    changed stationnavigator to avoid code repetition, fixed shadow on main navigation panels

--- components/StationNavigator.js
+++ components/StationNavigator.js
@@ -2,90 +2,58 @@ import { AiOutlineArrowUp, AiOutlineArrowDown } from "react-icons/ai";
 import { Link } from "react-scroll";
 
 const StationNavigator = ({ isDark, up, down }) => {
+	const mapArrToSign = (myArray, direction) => {
+		if(!myArray) return <></>
+
+		return (
+			<div className="lg:p-2 md:p-1 p-2 flex gap-1 items-center align-center overflow-hidden">
+				{direction === "UP" ? (
+					<AiOutlineArrowUp className="h-[100%] lg:min-w-[80px] md:min-w-[50px] min-w-[80px] text-[50px]" />
+				) : (
+					<AiOutlineArrowDown className="h-[100%] lg:min-w-[80px] md:min-w-[50px] min-w-[80px] text-[50px]" />
+				)}
+				{myArray.map((s, i) => {
+					return (
+						<Link
+							activeClass="active"
+							to={s.to}
+							spy={true}
+							smooth={true}
+							offset={0}
+							duration={500}
+							className="cursor-pointer flex gap-1 items-center align-center"
+							key={i}
+						>
+							<div className="dark:bg-white rounded-lg flex items-center justify-center text-center p-1">
+								<div
+									style={{ borderColor: isDark ? s.dark : s.light }}
+									className="text-slate-900 h-[38px] w-[38px] leading-[38px] rounded-full sm:text-[30px] text-[28px] font-bold dark:bg-white sm:border-[12px] border-[10px] box-content"
+								>
+									{s.title[0].toUpperCase()}
+								</div>
+							</div>
+							<div className="overflow-hidden whitespace-nowrap text-[35px]">
+								{s.title.length > 12 ? (
+									<p className="md:animate-none animate-scrollText md:p-left[0%] p-left-[100%] sm:block hidden max-h-[100px] md:max-w-none max-w-[300px]">
+										<strong>{s.title}</strong>
+									</p>
+								) : (
+									<p className="font-bold sm:block hidden max-h-[100px]">
+										{s.title}
+									</p>
+								)}
+							</div>
+						</Link>
+					);
+				})}
+			</div>
+		);
+	};
+
 	return (
 		<div className="border-slate-900 dark:border-black border-x-[7px] border-y-[1px]  flex lg:gap-2 gap-1 md:justify-start justify-around items-center align-center bg-slate-100 dark:bg-slate-900 text-slate-900 dark:text-white shadow-[inset_0_0_7px_0px_gray] dark:shadow-[inset_0_0_7px_1px_black]">
-			{!up ? (
-				""
-			) : (
-				<div className="lg:p-2 md:p-1 p-2 flex gap-1 items-center align-center overflow-hidden">
-					<AiOutlineArrowUp className="h-[100%] lg:min-w-[80px] md:min-w-[50px] min-w-[80px] text-[50px]" />
-					{up.map((s,i) => {
-						return (
-							<Link
-						activeClass="active"
-						to={s.to}
-						spy={true}
-						smooth={true}
-						offset={0}
-						duration={500}
-						className="cursor-pointer flex gap-1 items-center align-center"
-						key={i}
-					>
-								<div className="dark:bg-white rounded-lg flex items-center justify-center text-center p-1">
-									<div
-										style={{ borderColor: isDark ? s.dark : s.light }}
-										className="text-slate-900 h-[38px] w-[38px] leading-[38px] rounded-full sm:text-[30px] text-[28px] font-bold dark:bg-white sm:border-[12px] border-[10px] box-content"
-									>
-										{s.title[0].toUpperCase()}
-									</div>
-								</div>
-								<div className="overflow-hidden whitespace-nowrap text-[35px]">
-									{s.title.length > 12 ? (
-										<p className="md:animate-none animate-scrollText md:p-left[0%] p-left-[100%] sm:block hidden max-h-[100px] md:max-w-none max-w-[300px]">
-											<strong>{s.title}</strong>
-										</p>
-									) : (
-										<p className="font-bold sm:block hidden max-h-[100px]">
-											{s.title}
-										</p>
-									)}
-								</div>
-							</Link>
-						);
-					})}
-				</div>
-			)}
-			{!down ? (
-				""
-			) : (
-				<div className="p-2 flex gap-1 items-center align-center overflow-hidden">
-					<AiOutlineArrowDown className="h-[100%] lg:min-w-[80px] md:min-w-[50px] min-w-[80px] text-[50px]" />
-					{down.map((s,i) => {
-						return (
-							<Link
-						activeClass="active"
-						to={s.to}
-						spy={true}
-						smooth={true}
-						offset={0}
-						duration={500}
-						className="cursor-pointer flex gap-1 items-center align-center"
-						key={i}
-					>
-								<div className="dark:bg-white rounded-lg flex items-center justify-center text-center p-1">
-									<div
-										style={{ borderColor: isDark ? s.dark : s.light }}
-										className="text-slate-900 h-[38px] w-[38px] leading-[38px] rounded-full sm:text-[30px] text-[28px] font-bold dark:bg-white sm:border-[12px] border-[10px] box-content"
-									>
-										{s.title[0].toUpperCase()}
-									</div>
-								</div>
-								<div className="overflow-hidden whitespace-nowrap text-[35px]">
-									{s.title.length > 12 ? (
-										<p className="md:animate-none animate-scrollText md:p-left[0%] p-left-[100%] sm:block hidden  md:max-w-none max-h-[100px] max-w-[300px]">
-											<strong>{s.title}</strong>
-										</p>
-									) : (
-										<p className=" font-bold sm:block hidden max-h-[100px]">
-											{s.title}
-										</p>
-									)}
-								</div>
-							</Link>
-						);
-					})}
-				</div>
-			)}
+			{mapArrToSign(up, "UP")}
+			{mapArrToSign(down, "DOWN")}
 		</div>
 	);
 };

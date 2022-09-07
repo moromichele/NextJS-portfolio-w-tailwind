3485fe001d98d2d6f909b9848f8c709045a7db60
moromichele <moro.michele1997@gmail.com>
2022-09-07 14:18:28 +0200

    better navigation with keyboard

--- components/BigNavObject.js
+++ components/BigNavObject.js
@@ -1,10 +1,15 @@
 import { Link } from "react-scroll";
 import { AiOutlineArrowRight } from "react-icons/ai";
+import React, { useRef } from "react";
 
 const BigNavObject = ({ station, isDark }) => {
 	const { dark, light, title, to } = station;
-
+	const inputRef = React.useRef(null);
 	const bgColorReal = isDark ? dark : light;
+
+	const handleKeyDown = (e) => {
+		console.log(inputRef);
+	};
 
 	return (
 		<li className="">
@@ -21,20 +26,26 @@ const BigNavObject = ({ station, isDark }) => {
 					duration={500}
 					delay={1}
 					aria-label={`Go to ${title}`}
+					
+					ref={inputRef}
 				>
-					<div className="flex flex-col items-end h-full">
-						<div className="md:h-[240px] h-[30vh]"></div>
-						<AiOutlineArrowRight className="md:text-8xl text-7xl" />
-						<div className="border-2 border-white rounded-full">
-							<div
-								style={{ borderColor: "unset", color: bgColorReal }}
-								className="h-[35px] w-[35px] leading-[35px] rounded-full text-[30px] font-bold bg-white border-[12px] box-content"
-							>
-								{title[0].toUpperCase()}
+					<button className="w-full h-full" aria-label={`Go to ${title}`}>
+						<div className="flex flex-col items-end h-full">
+							<div className="md:h-[240px] h-[30vh]"></div>
+							<AiOutlineArrowRight className="md:text-8xl text-7xl" />
+							<div className="border-2 border-white rounded-full">
+								<div
+									style={{ borderColor: "unset", color: bgColorReal }}
+									className="h-[35px] w-[35px] leading-[35px] rounded-full text-[30px] font-bold bg-white border-[12px] box-content"
+								>
+									{title[0].toUpperCase()}
+								</div>
 							</div>
+							<p className="font-istok sm:text-2xl xsm:text-[22px] text-base">
+								{title}
+							</p>
 						</div>
-						<p className="font-istok sm:text-2xl xsm:text-[22px] text-base">{title}</p>
-					</div>
+					</button>
 				</Link>
 			</div>
 		</li>

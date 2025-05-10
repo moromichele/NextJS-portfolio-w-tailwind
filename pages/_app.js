093250384ba92283f3a910f0bf5324706a67037b
moromichele <moro.michele1997@gmail.com>
2025-05-10 19:25:26 +0200

    added next analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import "../styles/globals.css"
 import { ThemeProvider } from "/context/ThemeContext"
 import { Istok_Web } from "next/font/google"
+import { Analytics } from '@vercel/analytics/next';
 
 const istok = Istok_Web({
 	subsets: ["latin"],
@@ -14,6 +15,7 @@ function MyApp({ Component, pageProps }) {
 		<div className={istok.className}>
 			<ThemeProvider>
 				<Component {...pageProps} />
+				<Analytics />
 			</ThemeProvider>
 		</div>
 	)
